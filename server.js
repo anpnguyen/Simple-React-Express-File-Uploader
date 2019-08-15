5315957c17c2a9e6341abb0fb613bf5c8ef3d675
PleatherShaman <shamanwebsolutions@gmail.com>
2019-08-15 17:45:52 +1000

    add basic file upload

--- server.js
+++ server.js
@@ -7,23 +7,22 @@ const app = express();
 app.use(fileUpload());
 
 
-app.post("/upload", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No file uploaded" });
-  }
-  console.log()
-  const file = req.files.image;
-
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+app.post('/upload', (req, res) => {
+    if (req.files === null) {
+      return res.status(400).json({ msg: 'No file uploaded' });
     }
-
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  
+    const file = req.files.file;
+  
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+  
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
   });
-});
-// as
 
 
-app.listen(process.env.PORT, () => console.log("Server Started..."));
+app.listen(process.env.PORT, () => console.log(`Server Started on port ${process.env.PORT}`));
